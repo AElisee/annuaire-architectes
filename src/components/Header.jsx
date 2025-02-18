52bd3ec709b807aa1d4a69833f7843bd7c16ac7b
AElisee <2kange.eli@gmail.com>
2025-02-18 07:21:46 +0000

    ajout bg-image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 const Header = () => {
   return (
-    <header className="h-[100vh] w-full bg-gradient-to-b from-slate-600 to-zinc-600">
-      <div className="h-full flex justify-center items-center">
-        <h1 className="w-[90vw] md:leading-18 md:text-5xl text-3xl uppercase font-extrabold text-center pb-10 text-zinc-50">
-          annuaire de l'odre des architectes de côte d'ivoire
+    <header
+      className="h-[100vh] w-full bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/bg.jpeg')" }}
+    >
+      <div className="h-full flex justify-center items-center bg-black/50">
+        <h1 className="w-[90vw] md:leading-18 md:text-5xl text-3xl uppercase font-extrabold text-center pb-10  text-white drop-shadow-sm ">
+          annuaire de l'ordre des architectes de côte d'ivoire
         </h1>
       </div>
     </header>
